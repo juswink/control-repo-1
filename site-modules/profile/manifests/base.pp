67681a5ead96984065c2c2d8dbfd943ebd94b97f
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-16 17:47:31 +0800

    Update base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -2,8 +2,6 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
   
-  include motd
-  
   class { 'chrony':
     servers => lookup('ntpservers'),
   }
