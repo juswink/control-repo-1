add050696dd9c882b1f46fe3d78439a2b6798e2e
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-16 17:40:23 +0800

    Update base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -3,10 +3,9 @@ class profile::base {
   #the base profile should include component modules that will be on all nodes
   
   include motd
-  #include chrony
   
   class { 'chrony':
-   # servers => lookup('ntpservers'),
-   servers => [ '0.sg.pool.ntp.org', '1.sg.pool.ntp.org' ],
+    servers => lookup('ntpservers'),
+    #servers => [ '0.sg.pool.ntp.org', '1.sg.pool.ntp.org' ],
   }
 }
