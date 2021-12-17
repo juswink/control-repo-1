cfe8e8684cc5625a1f7c51f11936dbf26e96f0c2
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-17 17:46:14 +0800

    Update hiera.pp

--- site-modules/profile/manifests/hiera.pp
+++ site-modules/profile/manifests/hiera.pp
@@ -1,4 +1,4 @@
-class profile::hiera' {
+class profile::hiera {
   hiera_version   =>  '5',
           hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
           hierarchy       =>  [
