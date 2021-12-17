78e19fcdefa104e10310b0e1d8dfb2e752c988a1
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-17 17:03:40 +0800

    Update master_hiera.pp

--- site-modules/profile/manifests/master_hiera.pp
+++ site-modules/profile/manifests/master_hiera.pp
@@ -1,9 +1,10 @@
 class { 'hiera':
-  hierarchy => [
-    'secure',
-    '%{fqdn}',
-    '%{environment}',
-    'common'
-  ],
+  hiera_version   =>  '5',
+          hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
+          hierarchy       =>  [
+                                {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
+                                {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
+                                {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
+                              ],
   eyaml => true,
 }
