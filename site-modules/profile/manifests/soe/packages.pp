def9f5a60f66333bfb8ff4fac1c01270508714c9
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-20 18:33:22 +0800

    Update packages.pp

--- site-modules/profile/manifests/soe/packages.pp
+++ site-modules/profile/manifests/soe/packages.pp
@@ -1,6 +1,10 @@
 class profile::soe::packages {
+  
+  $facts['kernel'] == 'windows' {
+      include chocolatey
+  }
   $packages = lookup('packages')
-  $packages.each |String $package| {
+  $packages.each |String $package| {    
     package { $package:
       ensure => latest,
     }
