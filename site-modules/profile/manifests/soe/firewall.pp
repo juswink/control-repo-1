5389c944e241d178ad885a56f7b7866aca66f3e1
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-20 16:10:31 +0800

    Update firewall.pp

--- site-modules/profile/manifests/soe/firewall.pp
+++ site-modules/profile/manifests/soe/firewall.pp
@@ -1,2 +1,25 @@
 class profile::soe::firewall {
+
+  firewall { '000 accept all icmp requests':
+    proto  => 'icmp',
+    action => 'accept',
+  }
+  
+  case $facts['kernel'] {
+    'Linux':  {
+        firewall { '010 Allow inbound SSH':
+          dport    => 22,
+          proto    => 'tcp',
+          action   => 'accept',
+        }       
+    }
+    'windows':  {
+        firewall { '020 Allow inbound RDP':
+          dport    => 3389,
+          proto    => 'tcp',
+          action   => 'accept',
+        }       
+    }
+    default: { notify { "Too bad, so sad!\n" } }
+  }  
 }
