8e5dd5a2955414a686832d10576ce607e0845a3c
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-20 15:14:38 +0800

    Update ntp.pp

--- site-modules/profile/manifests/soe/ntp.pp
+++ site-modules/profile/manifests/soe/ntp.pp
@@ -1,2 +1,26 @@
 class profile::soe::ntp {
+
+  case $facts['kernel'] {
+    'Linux': {
+      case $facts['operatingsystem']['operatingsystemmajrelease'] {
+        'CentOS', '8':  { 
+          class { 'chrony':
+             servers => lookup('ntpservers'),
+          }
+        }
+        'CentOS', '7':  { 
+          class { 'ntp':
+             servers => lookup('ntpservers'),
+          }
+        }
+      }
+    }
+    'windows:  { 
+        class { 'winntp':
+           servers => lookup('ntpservers'),
+        }
+     }
+    
+  default:  { notify { "Too bad, so sad!\n" }
+  }
 }
