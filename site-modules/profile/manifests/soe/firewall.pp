cde4ec1d2c8c792a006134fc29a46982b5e9fb34
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-20 17:04:03 +0800

    Update firewall.pp

--- site-modules/profile/manifests/soe/firewall.pp
+++ site-modules/profile/manifests/soe/firewall.pp
@@ -14,11 +14,33 @@ class profile::soe::firewall {
         }       
     }
     'windows':  {
-        firewall { '020 Allow inbound RDP':
-          dport    => 3389,
-          proto    => 'tcp',
-          action   => 'accept',
-        }       
+        class { 'windows_firewall':
+          ensure => 'running',
+        }
+        
+        windows_firewall::exception { 'WINRM':
+          ensure       => present,
+          direction    => 'in',
+          action       => 'allow',
+          enabled      => true,
+          protocol     => 'TCP',
+          local_port   => 5985,
+          remote_port  => 'any',
+          display_name => 'Windows Remote Management HTTP-In',
+          description  => 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5985]',
+        }
+        
+        windows_firewall::exception { 'RDP':
+          ensure       => present,
+          direction    => 'in',
+          action       => 'allow',
+          enabled      => true,
+          protocol     => 'TCP',
+          local_port   => 3389,
+          remote_port  => 'any',
+          display_name => 'Remote Desktop Protocol-In',
+          description  => 'Inbound rule for Remote Desktop Protocol. [TCP 3389]',
+        }
     }
     default: { fail ( "Too bad, so sad!\n" ) }
   }
