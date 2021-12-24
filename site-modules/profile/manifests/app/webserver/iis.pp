6b14a47c8f25d278d78b6c5cdb2236cbf8647258
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-24 16:51:30 +0800

    Update iis.pp

--- site-modules/profile/manifests/app/webserver/iis.pp
+++ site-modules/profile/manifests/app/webserver/iis.pp
@@ -1,8 +1,7 @@
 class profile::app::webserver::iis {
-  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools', 'Web-Management-Console']
 
   iis_feature { $iis_features:
     ensure => 'present',
-    include_management_tools
   }
 }
