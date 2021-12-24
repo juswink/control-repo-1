32eb4943063e4cb6ff9143654465f15ba1f48f7a
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-24 15:59:39 +0800

    Update nginx.pp

--- site-modules/profile/manifests/app/webserver/nginx.pp
+++ site-modules/profile/manifests/app/webserver/nginx.pp
@@ -2,6 +2,6 @@ class profile::app::webserver::nginx {
 
   nginx::resource::server { 'sample_website':
     listen_port => 8080,
-    www_root => '/var/www/sample_website
+    www_root => '/var/www/sample_website',
   }
 }
