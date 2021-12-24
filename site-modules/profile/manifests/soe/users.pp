f01d5d9be0baa2a204451f3214ae0993a7bbf4d4
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-24 11:44:24 +0800

    Update users.pp

--- site-modules/profile/manifests/soe/users.pp
+++ site-modules/profile/manifests/soe/users.pp
@@ -1,8 +1,17 @@
 class profile::soe::users {
   $users = lookup('users')
   $users.each |String $user| {
-    user { $user:
-      ensure => present,
+    if $facts['kernel'] == 'Linux' {
+      user { $user:
+        ensure => present,
+      }  
     }
+    if $facts['kernel'] == 'windows' {
+      user { $user:
+        ensure => present,
+        name => $user,
+        groups => 'Users'
+      }  
+    }  
   }
 }
