4a2d3ab84431d2286ea2d852e102f6b8ff2e5d5e
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-28 17:27:08 +0800

    Update iis.pp

--- site-modules/profile/manifests/app/webserver/iis.pp
+++ site-modules/profile/manifests/app/webserver/iis.pp
@@ -10,29 +10,32 @@ class profile::app::webserver::iis {
     #require => iis_feature['Web-WebServer'],
   }
   
-  iis_site { 'minimal':
-    ensure          => 'started',
-    physicalpath    => 'c:\\inetpub\\minimal',
-    applicationpool => 'DefaultAppPool',
-    #require         => [
-    #  File['minimal'],
-    #  iis_site['Default Web Site']
-    #],
-  }
-
-  file { 'minimal':
-    ensure => 'directory',
-    path   => 'c:\\inetpub\\minimal',
-  }
-  
   $website_hash = lookup('iis_website::settings')
   
   $website_hash.each | $website | {
     $website_name = $website[1][website_name]
     $application_pool = $website[1][application_pool]
     $physical_path = $website[1][physical_path]
-    notify { $website_name: }
-    notify { $application_pool: }
-    notify { $physical_path: }
+    iis_site { $website_name:
+      ensure          => 'started',
+      physicalpath    => $physical_path,
+      applicationpool => $application_pool,
+    }
+    
+    file { $website_name:
+      ensure => 'directory',
+      path   => $physical_path,
+    }
   }
+  
+  #iis_site { 'minimal':
+  #  ensure          => 'started',
+  #  physicalpath    => 'c:\\inetpub\\minimal',
+  #  applicationpool => 'DefaultAppPool',
+    #require         => [
+    #  File['minimal'],
+    #  iis_site['Default Web Site']
+    #],
+  #}
+  
 }
