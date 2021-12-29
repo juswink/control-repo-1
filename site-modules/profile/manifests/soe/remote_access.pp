9fb3b5a225bcbc7f4e20fdd45835ab8743fe1b14
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-29 10:04:30 +0800

    Update remote_access.pp

--- site-modules/profile/manifests/soe/remote_access.pp
+++ site-modules/profile/manifests/soe/remote_access.pp
@@ -1,7 +1,7 @@
 class profile::soe::remote_access {
    case $facts['kernel'] {
      'Linux': {
-       ssh::sshd_config_allowusers {}
+       #ssh::sshd_config_allowusers {}
      }
      'windows': {
      }
