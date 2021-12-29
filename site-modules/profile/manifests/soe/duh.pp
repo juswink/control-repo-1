d85731785e8cb49806d7706ad3504c3fc8a888df
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2021-12-29 16:26:43 +0800

    Delete duh.pp

--- site-modules/profile/manifests/soe/duh.pp
+++
@@ -1,7 +0,0 @@
-class profile::soe::duh {
-  file_line { 'sshd_config':
-    ensure            => absent,
-    path              => '/etc/ssh/sshd_config',
-    line             => 'AllowUsers tom jerry centos',
-  }
-}
