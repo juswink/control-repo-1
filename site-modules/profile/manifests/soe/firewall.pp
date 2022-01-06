b6df954cca38181fe8d71822cff56649a1955bf8
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-06 18:57:38 +0800

    Removed whitespaces

--- site-modules/profile/manifests/soe/firewall.pp
+++ site-modules/profile/manifests/soe/firewall.pp
@@ -4,13 +4,13 @@ class profile::soe::firewall {
     proto  => 'icmp',
     action => 'accept',
   }
-  
+
   case $facts['kernel'] {
     'Linux':  {
         firewall { '010 Allow inbound SSH':
-          dport    => 22,
-          proto    => 'tcp',
-          action   => 'accept',
+          dport  => 22,
+          proto  => 'tcp',
+          action => 'accept',
         }
         #firewall { '020 Allow inbound HTTP':
         #  dport    => 80,
@@ -22,7 +22,7 @@ class profile::soe::firewall {
         class { 'windows_firewall':
           ensure => 'running',
         }
-        
+
         windows_firewall::exception { 'WINRM':
           ensure       => present,
           direction    => 'in',
@@ -34,7 +34,7 @@ class profile::soe::firewall {
           display_name => 'Windows Remote Management HTTP-In',
           description  => 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5985]',
         }
-        
+
         windows_firewall::exception { 'RDP':
           ensure       => present,
           direction    => 'in',
@@ -46,7 +46,7 @@ class profile::soe::firewall {
           display_name => 'Remote Desktop Protocol-In',
           description  => 'Inbound rule for Remote Desktop Protocol. [TCP 3389]',
         }
-        
+
         #windows_firewall::exception { 'HTTP':
         #  ensure       => present,
         #  direction    => 'in',
@@ -61,5 +61,5 @@ class profile::soe::firewall {
     }
     default: { notify { "Too bad, so sad!\n": } }
   }
-  
+
 }
