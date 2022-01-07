1e1840fdeeccf5e8ef32ecd3deb3c49e9cadae4b
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-07 18:38:04 +0800

    Removed firewall

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -2,7 +2,7 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
-  include profile::soe::firewall
+  #include profile::soe::firewall
   #include profile::soe::motd
   #include profile::soe::ntp
   include profile::soe::remote_access
