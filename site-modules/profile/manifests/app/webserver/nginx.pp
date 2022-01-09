a429f34abd0cf2b249dfd3a18013b7f606526355
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-09 21:08:30 +0800

    Commented out nginx

--- site-modules/profile/manifests/app/webserver/nginx.pp
+++ site-modules/profile/manifests/app/webserver/nginx.pp
@@ -1,13 +1,13 @@
 class profile::app::webserver::nginx {
 
-  include nginx
+  #include nginx
 
   file { "/var/www/${facts}['fqdn']/html/index.html":
     ensure  => file,
     content => '<html>I love Puppet!</html>',
   }
-  nginx::resource::server { "${facts}['fqdn']":
-    listen_port => 8080,
-    www_root    => "/var/www/${facts}['fqdn']/html",
-  }
+  #nginx::resource::server { "${facts}['fqdn']":
+  #  listen_port => 8080,
+  #  www_root    => "/var/www/${facts}['fqdn']/html",
+  #}
 }
