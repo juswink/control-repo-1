6eab84bd9311d0f1eac0a88b733ff6635e9036cc
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-09 15:09:01 +0800

    Added website

--- site-modules/profile/manifests/app/webserver/nginx.pp
+++ site-modules/profile/manifests/app/webserver/nginx.pp
@@ -2,12 +2,12 @@ class profile::app::webserver::nginx {
 
   include nginx
 
-  file { "/var/www/$facts['hostname']/index.html":
+  file { "/var/www/${facts}['hostname']/index.html":
     ensure  => file,
     content => '<html>I love Puppet!</html>',
   }
-  #nginx::resource::server { 'vhost1.com':
-  #  listen_port => 8080,
-  #  www_root => '/var/www/vhost1.com/html',
-  #}
+  nginx::resource::server { "${facts}['fqdn']":
+    listen_port => 8080,
+    www_root    => "/var/www/${facts}['fqdn']/html",
+  }
 }
