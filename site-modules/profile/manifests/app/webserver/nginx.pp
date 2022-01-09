5ff6a8e5efa882990c08cbbcd4e2884fe97ab958
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-09 22:00:05 +0800

    Removed nginx

--- site-modules/profile/manifests/app/webserver/nginx.pp
+++ site-modules/profile/manifests/app/webserver/nginx.pp
@@ -2,10 +2,10 @@ class profile::app::webserver::nginx {
 
   #include nginx
 
-  file { "/var/www/${facts}['fqdn']/html/index.html":
-    ensure  => file,
-    content => '<html>I love Puppet!</html>',
-  }
+  #file { "/var/www/${facts}['fqdn']/html/index.html":
+  #  ensure  => file,
+  #  content => '<html>I love Puppet!</html>',
+  #}
   #nginx::resource::server { "${facts}['fqdn']":
   #  listen_port => 8080,
   #  www_root    => "/var/www/${facts}['fqdn']/html",
