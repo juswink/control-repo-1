7effcf1fb8760060c521172ffd212db19c026a6b
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-10 18:57:57 +0800

    testing

--- site-modules/profile/manifests/app/webserver/nginx.pp
+++ site-modules/profile/manifests/app/webserver/nginx.pp
@@ -2,12 +2,15 @@ class profile::app::webserver::nginx {
 
   include nginx
 
-  $webservers_hash = lookup('nginx::nginx_servers', Hash, {'strategy' => 'unique'}, {})
+  $webservers_hash = lookup('nginx::nginx_servers')#, Hash, {'strategy' => 'unique'}, {})
   $webservers_hash.each | String $domain, Hash $options | {
-    file { "/var/www/${domain}/index.html":
-      ensure  => file,
-      content => '<html>I love Puppet!</html>',
-    }
+    #file { "/var/www/${domain}/index.html":
+    #  ensure  => file,
+    #  content => '<html>I love Puppet!</html>',
+    notify { "${domain}\n":}
+    notify { "${options}[www_root]\n":}
+    notify { "${options}[listen_port]\n":}
+  }
   #  nginx::resource::server { $domain:
   #    listen_port => $options[listen_port],
   #    www_root    => $options[www_root],
