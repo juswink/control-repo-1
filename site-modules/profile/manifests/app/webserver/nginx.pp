b3bcdf1adef79f014668be6a775af3c8540038c3
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-10 09:17:50 +0800

    Added a webpage

--- site-modules/profile/manifests/app/webserver/nginx.pp
+++ site-modules/profile/manifests/app/webserver/nginx.pp
@@ -2,7 +2,7 @@ class profile::app::webserver::nginx {
 
   include nginx
 
-  $webservers_hash = lookup('ngiinx_servers',Hash,'unique',{})
+  $webservers_hash = lookup('nginx_servers',Hash,'unique',{})
   $webservers_hash.each | String $domain, Hash $options | {
     file { "/var/www/${domain}/index.html":
       ensure  => file,
