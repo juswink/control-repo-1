b2a6d6793972ebccc2e2924c35652279853fbf4c
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-10 09:38:38 +0800

    Added a webpage

--- site-modules/profile/manifests/app/webserver/nginx.pp
+++ site-modules/profile/manifests/app/webserver/nginx.pp
@@ -2,15 +2,15 @@ class profile::app::webserver::nginx {
 
   include nginx
 
-  $webservers_hash = lookup('nginx_servers',undef,'unique',{})
+  $webservers_hash = lookup('nginx::nginx_servers',Hash,'unique',{})
   $webservers_hash.each | String $domain, Hash $options | {
     file { "/var/www/${domain}/index.html":
       ensure  => file,
       content => '<html>I love Puppet!</html>',
     }
-    nginx::resource::server { $domain:
-      listen_port => $options[listen_port],
-      www_root    => $options[www_root],
-    }
-  }
+  #  nginx::resource::server { $domain:
+  #    listen_port => $options[listen_port],
+  #    www_root    => $options[www_root],
+  #  }
+  #}
 }
